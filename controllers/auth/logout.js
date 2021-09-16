4118e25c6c2fb4536bee3be15887d3c676543c30
Sergii Maksymenko <sergiomaksymenko@gmail.com>
2021-09-16 16:58:24 +0200

    some fix

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,7 +1,6 @@
 const { User } = require('../../models')
 
 const logout = async (req, res, _next) => {
-  await console.log(req.user)
   await User.findOneAndUpdate(req.user._id, { token: null })
   res.status(200).json({ message: 'Success logout' })
 }
